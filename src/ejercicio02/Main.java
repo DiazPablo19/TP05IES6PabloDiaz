8093864f612893b4a8f5deac48926e5a2d99a715
Díaz Pablo Ismael <diazpabloismael0@gmail.com>
2025-09-05 05:01:36 -0300

    Segunda parte del TP05 IES6

--- src/ejercicio02/Main.java
+++ src/ejercicio02/Main.java
@@ -1,5 +1,22 @@
 package ejercicio02;
-
+import java.util.Scanner;
 public class Main {
+ public static void main(String[] args) throws Exception {
+  Scanner sc = new Scanner(System.in);
+  System.out.println("Ingrese un numero: ");
+  int numero = sc.nextInt();
+
+  if (numero%2==0){
+    System.out.println("El numero es par. El triple es: " + (numero*3));
+  } else {
+     System.out.println("El numero es impar. El doble es: " + (numero*2));
+  }
+
+  sc.close();
+
+
+
+
+}
 
 }
