5c0dcbd8f18bc8197f0644f42e3a98b89e799440
Díaz Pablo Ismael <diazpabloismael0@gmail.com>
2025-09-05 23:58:57 -0300

    Punto2 factorial y while TP05 IES6

--- src/ejercicio03/Main.java
+++ src/ejercicio03/Main.java
@@ -1,5 +1,30 @@
 package ejercicio03;
-
+import java.util.Scanner;
 public class Main {
+    public static void main(String[] args) throws Exception {
+        Scanner sc = new Scanner(System.in);
 
+        System.out.println("ingresar un numero entre 0 y 10: ");
+        int numero = sc.nextInt();
+       
+        int factorial = 1; 
+            int contador = numero;
+        if (numero < 0 || numero > 10) {
+            System.out.println("el numero debe estar entre 0 y 10: ");
+
+        } else {
+        if (numero==0) {
+          factorial = 1;
+        } else {
+          while (contador >= 1) {
+            factorial = factorial * contador;
+            contador--;
+            
+          }
+        }
+        System.out.println("el factorial de: " + numero + " es: " + factorial);
+    }
+sc.close();
+
+}
 }
