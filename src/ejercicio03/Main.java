0d4172fc6129337394c426fd6709d80b31dccfc8
Díaz Pablo Ismael <diazpabloismael0@gmail.com>
2025-09-06 03:10:19 -0300

    Punto 4 del TP05 del IES6 tablas de multiplicar y uso de for

--- src/ejercicio03/Main.java
+++ src/ejercicio03/Main.java
@@ -1,24 +1,24 @@
 package ejercicio03;
 import java.util.Scanner;
 public class Main {
-    public static void main(String[] args) throws Exception {
-        Scanner sc = new Scanner(System.in);
-
-        System.out.println("ingresar un numero entre 0 y 10: ");
-        int numero = sc.nextInt();
+ public static void main(String[] args) throws Exception {
+  Scanner sc = new Scanner(System.in);
+ 
+    System.out.println("ingresar un numero entre 0 y 10: ");
+    int numero = sc.nextInt();
        
-        int factorial = 1; 
-            int contador = numero;
+     int factorial = 1; 
+     int contador = numero;
         if (numero < 0 || numero > 10) {
-            System.out.println("el numero debe estar entre 0 y 10: ");
+        System.out.println("el numero debe estar entre 0 y 10: ");
 
         } else {
-        if (numero==0) {
+          if (numero==0) {
           factorial = 1;
-        } else {
-          while (contador >= 1) {
-            factorial = factorial * contador;
-            contador--;
+            } else {
+              while (contador >= 1) {
+              factorial = factorial * contador;
+              contador--;
             
           }
         }
--- src/ejercicio04/Main.java
+++ src/ejercicio04/Main.java
@@ -1,5 +1,20 @@
 package ejercicio04;
-
+import java.util.Scanner;
 public class Main {
-
+ public static void main(String[] args) throws Exception {
+     Scanner sc = new Scanner(System.in);
+     System.out.println("Ingrese un numero entre 1 y 9: ");
+     int numero = sc.nextInt(); 
+     if (numero<1 || numero>9){
+        System.out.println("El número ingresado está fuera de rango. ");
+     }else{
+       System.out.println("La tabla del " + numero + " es: ");
+       for (int i = 1; i<= 10; i++){ 
+         int resultado = numero * i;
+         System.out.println(numero + " x " + i + " = " + resultado);
+      }
+     sc.close();
+   }
+  }
 }
+
